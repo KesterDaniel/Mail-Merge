8efd1148e5c6c6ef8832713a292849f6df6fb43c
KesterDaniel <kesterdan17@gmail.com>
2023-01-28 16:10:31 +0100

    optimize code for readability

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 with open("./Input/Names/invited_names.txt") as invited_names:
     names = invited_names.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter:
+    full_letter = letter.read()
     for name in names:
-        with open("./Input/Letters/starting_letter.txt") as letter:
-            letter_lines = letter.readlines()
-            letter_lines[0] = letter_lines[0].replace("[name]", name.strip())
-            with open(f"./Output/ReadyToSend/{name.strip()}.txt", mode="w") as new_letter:
-                for line in letter_lines:
-                    new_letter.write(f"{line}")
+        new_letter = full_letter.replace("[name]", name.strip())
+        with open(f"./Output/ReadyToSend/{name.strip()}.txt", mode="w") as complete_letter:
+            complete_letter.write(new_letter)
 
 
