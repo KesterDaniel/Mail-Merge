dcb0367769bc8e282f27511c29a9374e3a5e9366
KesterDaniel <kesterdan17@gmail.com>
2023-01-28 15:40:03 +0100

    did some refactoring and final commit

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-with open("./Input/Letters/starting_letter.txt") as letter:
-    letter_lines = letter.readlines()
-    with open("./Input/Names/invited_names.txt") as invited_names:
-        names = invited_names.readlines()
-        for name in names:
-            letter_lines[0].replace("[name]", f"{name}")
-            
+with open("./Input/Names/invited_names.txt") as invited_names:
+    names = invited_names.readlines()
+    for name in names:
+        with open("./Input/Letters/starting_letter.txt") as letter:
+            letter_lines = letter.readlines()
+            letter_lines[0] = letter_lines[0].replace("[name]", name.strip())
+            with open(f"./Output/ReadyToSend/{name.strip()}.txt", mode="w") as new_letter:
+                for line in letter_lines:
+                    new_letter.write(f"{line}")
 
 
-
-
-
-
-
-# for line in letter_lines:
-    #     print(f"{line}")
-    # # print(letter_lines[0].replace("[name]", "Me"))
