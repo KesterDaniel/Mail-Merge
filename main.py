5a74b0e58e2410ccc62b3fc80c0b558e37395083
KesterDaniel <kesterdan17@gmail.com>
2023-01-28 14:32:58 +0100

    read lines from invited names

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 with open("./Input/Letters/starting_letter.txt") as letter:
     letter_lines = letter.readlines()
+    with open("./Input/Names/invited_names.txt") as invited_names:
+        names = invited_names.readlines()
     
