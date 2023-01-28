71e317aaed76e816a34b25ebb7724992fc9eccad
KesterDaniel <kesterdan17@gmail.com>
2023-01-28 14:42:27 +0100

    replace name place holder with invited name

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ with open("./Input/Letters/starting_letter.txt") as letter:
     letter_lines = letter.readlines()
     with open("./Input/Names/invited_names.txt") as invited_names:
         names = invited_names.readlines()
-    
+        for name in names:
+            letter_lines[0].replace("[name]", f"{name}")
+            
+
+
+
+
+
+
+
+# for line in letter_lines:
+    #     print(f"{line}")
+    # # print(letter_lines[0].replace("[name]", "Me"))
